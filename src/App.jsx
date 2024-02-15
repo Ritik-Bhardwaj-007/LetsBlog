bc1ae1a7cdee6bcf9648706395301dfe1746ae4e
Ritik Bhardwaj <ritiksharma0748@gmail.com>
2024-02-16 00:00:30 +0530

    Redux done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth"
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
+  const [loading,setLoading]=useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1> a blog App {import.meta.env.VITE_APPWRITE_URL}</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData));
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false));
+  },[])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+   test
+    </div>
+  ):null
 }
 
 export default App
