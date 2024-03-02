ed129f0b8411a6e990da7160c9f43a39e37d1f42
Ritik Bhardwaj <ritiksharma0748@gmail.com>
2024-03-03 01:18:35 +0530

    path updated

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,7 @@ import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from './Login';
 import PostForm from './Post-form/PostForm'
-import PostCard from './PostCard'
+import PostCard from './PostCard.jsx'
 import Button from './Button';
 import Input from './Input';
 import Select from './Select'
